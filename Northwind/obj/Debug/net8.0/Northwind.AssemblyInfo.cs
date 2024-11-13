c0c93a663e2f69527114f074b0f3c82c2992e661
Dr. Mary Lebens <mary@marylebens.com>
2024-11-13 22:35:03 +0000

    commit

--- Northwind/obj/Debug/net8.0/Northwind.AssemblyInfo.cs
+++ Northwind/obj/Debug/net8.0/Northwind.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6010b7d71385704dd4ed3adfcefd2a371a56173")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b462b03b4bb640850ab40786fdeea43e4a3812c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
