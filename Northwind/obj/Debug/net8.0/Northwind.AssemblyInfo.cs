0469030d63cf8cfe1eb538b8568877f160db7cdf
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2024-11-19 00:15:33 +0000

    Finally!

--- Northwind/obj/Debug/net8.0/Northwind.AssemblyInfo.cs
+++ Northwind/obj/Debug/net8.0/Northwind.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b462b03b4bb640850ab40786fdeea43e4a3812c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c93a663e2f69527114f074b0f3c82c2992e661")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
